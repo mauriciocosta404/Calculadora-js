5c4dc0d4616f189cc7d01d6a6c3f4553aaa4a598
menezescosta <mc611641@gmail.com>
2022-04-13 00:25:24 +0100

    projecto calculadora com js feito em um dia
    projecto calculadora com js

---
+++ calculadora js/src/js/script.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const buttons=document.querySelectorAll(".button");
+
+buttons.forEach((item)=>{
+
+    item.addEventListener('click',(event)=>{
+        inserir(item.value);
+    })
+})
+
+function inserir(value) {
+    if(value==="C"){
+        clear();
+    }
+    else if(value==="<"){
+        back();
+    }
+    else if(value==="="){
+        calculate();
+    }
+    else{
+         let number=document.querySelector("#result").innerHTML+=value;
+    }
+}
+
+function clear() {
+    document.querySelector("#result").innerHTML="";
+}
+
+function back() {
+    let number=document.querySelector("#result").innerHTML;
+
+    document.querySelector("#result").innerHTML=number.substring(0,number.length-1);
+}
+
+function calculate(){
+    let result=document.querySelector("#result").innerHTML;
+
+    if(result){
+        document.querySelector("#result").innerHTML=eval(result);
+    }
+}
